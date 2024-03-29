53df16efacdec31b88e96d967b67e64f8c9e39bb
Manisha Pillai <pillaimanisha77@gmail.com>
2024-03-29 22:54:49 +0530

    Initial commit

---
+++ imports/ui/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import { Hello } from './Hello.jsx';
+// import { Info } from './Info.jsx';
+
+export const App = () => (
+  <section class="hero is-fullheight">
+    <div class='hero-body'>
+      <div class='container has-text-centered'>
+        <p class='title'>Tic Tac Toe</p>
+        <div class='button'>New Game</div>
+        <br></br>
+        <div class='button'>Join Game</div>
+      </div>
+    </div>
+  </section>
+);
